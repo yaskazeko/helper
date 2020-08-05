9ef1b7f4c7783c64c125d576eb6e42d4cda15d55
yaskazeko <yaskazeko@gmail.com>
2020-08-05 16:25:06 +0300

    make folder

---
+++ helper.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import os
+import sys
+
+if __name__ == "__main__":
+   dir = sys.argv[1] 
+print (dir)
+
+listFiles = os.listdir(dir)
+
+for f in listFiles:
+    print(f)
